504a29160955d8a5f4f636a3856148d9c1b950c4
Pradeep <tidkepradeep@gmail.com>
2022-06-19 11:27:40 +0530

    readme added

---
+++ README.md
@@ -0,0 +1,31 @@
+Aliianz logo detector
+
+Folder structure:
+
+data            # All images stored here
+    predict     # Images can be used for prediction    
+    raw         # Raw images used for augmentation
+    test        # Images used for validation
+    train       # images used for train and test
+model           # model directory
+    model.pkl   # trained model
+app.py          # flask application for web deployment
+augment.py      # augmentation
+lib.py          # library
+main.py         # tarin, test, evaluate, validate
+README.md       # Readme
+requirements.txt    # python libraries to be installed
+
+
+Prerequisites:
+NOTE: 
+This project has been tested on Windows OS, Python 3.10.5 interpreter. 
+
+Steps:
+1. Install Python 3.10.5
+2. cd REPO 
+3. pip install -r requirements
+4. python augment.py # create augemented images
+5. python main.py # modelling, train, test, evaluate
+
+
--- lib.py
+++ lib.py
@@ -1,3 +1,5 @@
+# this is library file
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from skimage import exposure, feature
@@ -6,47 +8,32 @@ import cv2 as cv
 import glob
 import os
 import joblib
-import imgaug.augmenters as iaa
 import sklearn
 import sys
-
+import platform as pf
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import shutil
 
 # Get Paths
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 head, _ = os.path.split(ROOT_DIR)
 
-# print(head,'\n',_)
+######## functions #########
 
-def image_augment(img_path):
-    # 1. Load Dataset
-    images = []
-    # images_path = glob.glob(f"{image_path}")
-    # for img_path in images_path:
-    img = cv.imread(img_path)
-    print(np.shape(img))
-    images.append(img)
-        
-    # 2. Image Augmentation
-    augmentation = iaa.Sequential([
-        # 1. Flip
-        iaa.Fliplr(0.5),
-        iaa.Flipud(0.5),
+# path divider per OS type
+def get_path_divider():
+    if pf.system()=='Windows':
+        path_div='\\'
+    else:
+        path_div='/'
+    return path_div
 
-        # 2. Affine
-        iaa.Affine(translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-                rotate=(-30, 30),
-                scale=(0.5, 1.5)),
+# absolute path
+def abspath(loc):
+    return os.path.abspath(loc)
 
-        # 3. Multiply
-        iaa.Multiply((0.8, 1.2)),
+############################
 
-        # 4. Linearcontrast
-        iaa.LinearContrast((0.6, 1.4)),
-
-        # Perform methods below only sometimes
-        iaa.Sometimes(0.5,
-            # 5. GaussianBlur
-            iaa.GaussianBlur((0.0, 3.0))
-            )
-    ])
-    return augmentation(images=images)
+path_div=get_path_divider()
+model_path=f'model{path_div}model.pkl'
